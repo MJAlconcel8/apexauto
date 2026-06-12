008941adb952a983079e5f550f234ac58e585d4c
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2026-06-12 09:49:40 -0400

    Chore: Initalizing Backend

---
+++ apexauto/src/test/java/com/example/apexauto/ApexautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apexauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
